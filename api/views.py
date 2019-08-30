fd9c602d1cf77a9aaacb54f69908887898865726
ricardo <ricardo.sikic@gmail.com>
2019-08-30 14:54:39 -0400

    initial changes

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
